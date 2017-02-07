6bf649ccd02509fd206a3fe94165073d00718b27
Julio Guilindro <julio.guilindro@gmail.com>
2017-02-07 12:43:57 -0500

    Mejorada identación en la clase Suma

--- GuilindroSum/src/main/java/Suma.java
+++ GuilindroSum/src/main/java/Suma.java
@@ -5,19 +5,19 @@ public class Suma {
 	 * Por Julio Guilindro para el examen de Ingenieria en software 2
 	 * @return Numeros enteros
 	 */
-	final int sumador(int[] numeros){
+    final int sumador(int[] numeros){
 		/**
 		 * @params int[]
 		 */
         int suma = 0;
         for (int i=0 ; i<numeros.length ; i++){
-			suma = suma + numeros[i];
-		}
-		return suma;
-	}
+            suma = suma + numeros[i];
+        }
+        return suma;
+    }
 	
 	int [] convertidor(String s){
-		return null;
-	}
+        return null;
+    }
 
 }
