b2da60d31671e37f91c602ccfdc2cae75d2773d0
Julio Guilindro <julio.guilindro@gmail.com>
2017-02-07 12:23:02 -0500

    Reportes de Checkstyle creado

--- GuilindroSum/src/main/java/Suma.java
+++ GuilindroSum/src/main/java/Suma.java
@@ -1,9 +1,17 @@
 
 public class Suma {
-	int sumador (int[] numeros){
-		int suma=0;
-		for (int i=0;i<numeros.length;i++){
-			suma= suma+numeros[i];
+	/**
+	 * Programa que me permite la suma de numeros ingresados como string
+	 * Por Julio Guilindro para el examen de Ingenieria en software 2
+	 * @return Numeros enteros
+	 */
+	final int sumador(int[] numeros){
+		/**
+		 * @params int[]
+		 */
+        int suma = 0;
+        for (int i=0 ; i<numeros.length ; i++){
+			suma = suma + numeros[i];
 		}
 		return suma;
 	}
